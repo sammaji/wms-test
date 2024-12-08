07e4750294c975e134ca23c6c333814ac8f8eae1
root <root@DylanBlackWork.>
2024-12-08 13:45:44 +1000

    feat: update container configuration for improved mobile responsiveness

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,17 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+      },
       screens: {
-        "2xl": "1400px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1536px",
+        "2xl": "1920px",
       },
     },
     extend: {

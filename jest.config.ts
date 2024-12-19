1bb11ced31a2d23b860b339da58ff47343a0557f
Dylan <46850979+dintyo@users.noreply.github.com>
2024-12-19 09:55:08 +0800

    Initial commit: Warehouse Management Test Task

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'jest'
+import type { Config } from '@jest/types'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   dir: './',
 })
 
-const config: Config = {
+const config: Config.InitialOptions = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
